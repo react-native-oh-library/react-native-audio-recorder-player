77a2f418b253a3f3770989f5c4360fd0c8643718
zy432099 <46698434+zy432099@users.noreply.github.com>
2024-12-19 15:40:18 +0800

    开源五项核查 (#11)
    Co-authored-by: 张宇 <478110507@qq.com>

--- harmony/audio_recorder_player/src/main/ets/recorder/types.ts
+++ harmony/audio_recorder_player/src/main/ets/recorder/types.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import media from '@ohos.multimedia.media';
 
 export const defaultBitrate: number = 48000;
